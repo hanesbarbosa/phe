b0d1b24c02e4d603bd9fe41781439d09eb749c7f
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-06 13:29:23 -0600

    Creating scalar multiplication function.

---
+++ scalar_multiplication.go
@@ -0,0 +1,17 @@
+package phe
+
+import "math/big"
+
+// ScalarMultiplication multiplies a multivector by a scalar.
+func ScalarMultiplication(m Multivector, s *big.Int, q *big.Int) Multivector {
+	m.E0.Mul(m.E0, s)
+	m.E1.Mul(m.E1, s)
+	m.E2.Mul(m.E2, s)
+	m.E3.Mul(m.E3, s)
+	m.E12.Mul(m.E12, s)
+	m.E13.Mul(m.E13, s)
+	m.E23.Mul(m.E23, s)
+	m.E123.Mul(m.E123, s)
+
+	return ModularMultivector(m, q)
+}
---
+++ scalar_multiplication_test.go
@@ -0,0 +1,28 @@
+package phe
+
+import (
+	"math/big"
+	"strings"
+	"testing"
+)
+
+// Tests if the scalar multiplication is calculated properly.
+func TestScalarMultiplication(t *testing.T) {
+	c := []string{"2", "3", "4", "5", "6", "7", "8", "9"}
+
+	m := New(c)
+	q := big.NewInt(257)
+	s := big.NewInt(12)
+	m = ScalarMultiplication(m, s, q)
+
+	if strings.Compare(m.E0.String(), "24") != 0 ||
+		strings.Compare(m.E1.String(), "36") != 0 ||
+		strings.Compare(m.E2.String(), "48") != 0 ||
+		strings.Compare(m.E3.String(), "60") != 0 ||
+		strings.Compare(m.E12.String(), "72") != 0 ||
+		strings.Compare(m.E13.String(), "84") != 0 ||
+		strings.Compare(m.E23.String(), "96") != 0 ||
+		strings.Compare(m.E123.String(), "108") != 0 {
+		t.Errorf("Wrong results for the scalar multiplication calculation.")
+	}
+}
