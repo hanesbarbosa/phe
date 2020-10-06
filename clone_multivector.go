dea96e5079cd2519d752f1fca81bc85d5195cffd
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-05 21:16:20 -0600

    Creating function to clone multivectors.

---
+++ clone_multivector.go
@@ -0,0 +1,16 @@
+package phe
+
+// CloneMultivector clones a multivector.
+func CloneMultivector(m Multivector) Multivector {
+	mc := New([]string{
+		m.E0.String(),
+		m.E1.String(),
+		m.E2.String(),
+		m.E3.String(),
+		m.E12.String(),
+		m.E13.String(),
+		m.E23.String(),
+		m.E123.String(),
+	})
+	return mc
+}
---
+++ clone_multivector_test.go
@@ -0,0 +1,45 @@
+package phe
+
+import (
+	"strings"
+	"testing"
+)
+
+// Tests if the cloned multivector keeps separated values.
+func TestCloneMultivector(t *testing.T) {
+	c := []string{"40", "-29", "29", "-25", "33", "-29", "32", "-28"}
+
+	m := New(c)
+	mc := CloneMultivector(m)
+
+	if strings.Compare(m.E0.String(), mc.E0.String()) != 0 ||
+		strings.Compare(m.E1.String(), mc.E1.String()) != 0 ||
+		strings.Compare(m.E2.String(), mc.E2.String()) != 0 ||
+		strings.Compare(m.E3.String(), mc.E3.String()) != 0 ||
+		strings.Compare(m.E12.String(), mc.E12.String()) != 0 ||
+		strings.Compare(m.E13.String(), mc.E13.String()) != 0 ||
+		strings.Compare(m.E23.String(), mc.E23.String()) != 0 ||
+		strings.Compare(m.E123.String(), mc.E123.String()) != 0 {
+		t.Errorf("Different results for cloned multivector when it should be equal.")
+	}
+
+	m.E0.SetString("1", 10)
+	m.E1.SetString("1", 10)
+	m.E2.SetString("1", 10)
+	m.E3.SetString("1", 10)
+	m.E12.SetString("1", 10)
+	m.E13.SetString("1", 10)
+	m.E23.SetString("1", 10)
+	m.E123.SetString("1", 10)
+
+	if strings.Compare(m.E0.String(), mc.E0.String()) == 0 ||
+		strings.Compare(m.E1.String(), mc.E1.String()) == 0 ||
+		strings.Compare(m.E2.String(), mc.E2.String()) == 0 ||
+		strings.Compare(m.E3.String(), mc.E3.String()) == 0 ||
+		strings.Compare(m.E12.String(), mc.E12.String()) == 0 ||
+		strings.Compare(m.E13.String(), mc.E13.String()) == 0 ||
+		strings.Compare(m.E23.String(), mc.E23.String()) == 0 ||
+		strings.Compare(m.E123.String(), mc.E123.String()) == 0 {
+		t.Errorf("Equal results for cloned multivector when it should be different.")
+	}
+}
