dd8e7111bfc190cbd0035be8022c485e6cf4f63d
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-06 20:09:33 -0600

    Adding boolean function HasInverse.

--- inverse.go
+++ inverse.go
@@ -1,6 +1,9 @@
 package phe
 
-import "math/big"
+import (
+	"math/big"
+	"strings"
+)
 
 // Inverse gives the inverse of a multivector.
 func Inverse(m Multivector, q *big.Int) Multivector {
@@ -20,15 +23,26 @@ func Numerator(m Multivector, q *big.Int) Multivector {
 	return GeometricProduct(cc, asr, q)
 }
 
-// // Denominator to be defined.
-// func Denominator(m Multivector) string {
-// 	r := Rationalize(m)
-// 	return r.E0.Num().String()
-// }
-
 // DenominatorInverse to be defined.
 func DenominatorInverse(m Multivector, q *big.Int) *big.Int {
 	d := Rationalize(m, q).E0
 	di := d.ModInverse(d, q)
 	return di
+}
+
+// HasInverse checks if a multivector (i.e: key) has inverse.
+// If so, the decryption can occur.
+func HasInverse(m Multivector, q *big.Int) bool {
+	m1 := CloneMultivector(m)
+	mi := Inverse(m1, q)
+	gp := GeometricProduct(m, mi, q)
+
+	return (strings.Compare(gp.E0.String(), "1") == 0 &&
+		strings.Compare(gp.E1.String(), "0") == 0 &&
+		strings.Compare(gp.E2.String(), "0") == 0 &&
+		strings.Compare(gp.E3.String(), "0") == 0 &&
+		strings.Compare(gp.E12.String(), "0") == 0 &&
+		strings.Compare(gp.E13.String(), "0") == 0 &&
+		strings.Compare(gp.E23.String(), "0") == 0 &&
+		strings.Compare(gp.E123.String(), "0") == 0)
 }
