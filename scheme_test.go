63f0fe80c61ced8004f5f4060275ebeb23b6d047
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-07 17:12:59 -0600

    Creating tests for encryption, decryption, addition and scalar division.

--- scheme_test.go
+++ scheme_test.go
@@ -1,41 +1,60 @@
 package phe
 
 import (
-	"fmt"
 	"math/big"
+	"strings"
 	"testing"
 )
 
-// TestGenerateKeys...
-func TestScheme(t *testing.T) {
-	sk, pk := GenerateKeys(128)
+func TestEncryptionDecryption(t *testing.T) {
+	sk, pk := GenerateKeys(256)
+
+	m1 := big.NewInt(300)
+
+	c1 := Encrypt(sk, pk, m1)
+
+	rm1 := Decrypt(sk, pk, c1)
+
+	if strings.Compare(m1.String(), rm1.Num().String()) != 0 {
+		t.Errorf("decyphered message does not match original message")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	sk, pk := GenerateKeys(256)
+
 	m1 := big.NewInt(110)
 	m2 := big.NewInt(190)
-	s := big.NewInt(8)
+
 	c1 := Encrypt(sk, pk, m1)
 	c2 := Encrypt(sk, pk, m2)
+
 	c3 := Addition(pk, c1, c2)
+
+	rm3 := Decrypt(sk, pk, c3)
+
+	if strings.Compare(rm3.Num().String(), "300") != 0 {
+		t.Errorf("decyphered result does not match the addition of the original numbers")
+	}
+}
+
+func TestSDiv(t *testing.T) {
+	sk, pk := GenerateKeys(256)
+
+	m1 := big.NewInt(110)
+	m2 := big.NewInt(190)
+	s := big.NewInt(7)
+
+	c1 := Encrypt(sk, pk, m1)
+	c2 := Encrypt(sk, pk, m2)
+
+	c3 := Addition(pk, c1, c2)
+
 	c4 := ScalarDivision(pk, c3, s)
-	// rm := Decrypt(sk, pk, c3)
-	rm := Decrypt(sk, pk, c4)
 
-	// p := big.NewInt(51)
-	// c := big.NewInt(14)
-	// f := ExtendedEuclideanAlgorithm(p, c)
-	// f := ExtendedEuclideanAlgorithm(pk.q, rm)
+	rm4 := Decrypt(sk, pk, c4)
 
-	// // SK
-	// fmt.Println("sk.k1 = ", sk.k1.ToString())
-	// fmt.Println("sk.k2 = ", sk.k2.ToString())
-	// fmt.Println("sk.g = ", sk.g.String())
-	// // PK
-	// fmt.Println("pk.b = ", pk.b)
-	// fmt.Println("pk.q = ", pk.q)
-	// fmt.Println("###################################")
-	// m
-	fmt.Println("retrieved message = ", rm.String())
-
-	// fmt.Println("###################################")
-	// fmt.Println("f = ", f.String())
-	// fmt.Println("###################################")
+	if strings.Compare(rm4.String(), "300/7") != 0 {
+		t.Errorf("decyphered result does not match the scalar division of the addition result")
+	}
 }
