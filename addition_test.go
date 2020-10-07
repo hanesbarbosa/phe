7eed8f3823064a98f215c2e384631823e550b175
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-06 22:18:21 -0600

    Creating function addition.

---
+++ addition_test.go
@@ -0,0 +1,28 @@
+package phe
+
+import (
+	"math/big"
+	"strings"
+	"testing"
+)
+
+// Tests the addition of two multivectors.
+func TestAddition(t *testing.T) {
+	pk := PK{q: big.NewInt(10)}
+
+	m1 := NewMultivector([]string{"2", "3", "4", "5", "6", "7", "8", "9"})
+	m2 := NewMultivector([]string{"2", "3", "4", "5", "6", "7", "8", "9"})
+
+	m := Addition(pk, m1, m2)
+
+	if strings.Compare(m.E0.String(), "4") != 0 ||
+		strings.Compare(m.E1.String(), "6") != 0 ||
+		strings.Compare(m.E2.String(), "8") != 0 ||
+		strings.Compare(m.E3.String(), "0") != 0 ||
+		strings.Compare(m.E12.String(), "2") != 0 ||
+		strings.Compare(m.E13.String(), "4") != 0 ||
+		strings.Compare(m.E23.String(), "6") != 0 ||
+		strings.Compare(m.E123.String(), "8") != 0 {
+		t.Errorf("The addition is not correct!")
+	}
+}
