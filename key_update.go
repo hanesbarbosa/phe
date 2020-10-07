d8c79132b400e01f15c5dbb1e897e104f4a1bc81
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-07 12:21:37 -0600

    Creating function for key update.

---
+++ key_update.go
@@ -0,0 +1,9 @@
+package phe
+
+// KeyUpdate substitutes the key pair s1 and s2 by s3 and s4 through tokens t1 and t2.
+func KeyUpdate(pk PublicKey, t Token, c1 Multivector) Multivector {
+	c2 := GeometricProduct(t.t1, c1, pk.q)
+	c2 = GeometricProduct(c2, t.t2, pk.q)
+
+	return c2
+}
