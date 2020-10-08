85f2f869946ea069c11f30b57a0ad80ba48d639f
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-08 11:38:51 -0600

    Creating a function to test token generation and key update.

--- scheme_test.go
+++ scheme_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestEncryptionDecryption(t *testing.T) {
+func TestEncDec(t *testing.T) {
 	sk, pk := GenerateKeys(256)
 
 	m1 := big.NewInt(300)
@@ -56,5 +56,25 @@ func TestSDiv(t *testing.T) {
 
 	if strings.Compare(rm4.String(), "300/7") != 0 {
 		t.Errorf("decyphered result does not match the scalar division of the addition result")
+	}
+}
+
+func TestTokGenKeyUpd(t *testing.T) {
+	sk1, pk1 := GenerateKeys(256)
+
+	m1 := big.NewInt(100)
+
+	c1 := Encrypt(sk1, pk1, m1)
+
+	sk2, pk2 := GenerateKeys(256)
+
+	tk := GenerateToken(sk1, sk2, pk1, pk2)
+
+	c2 := KeyUpdate(pk1, tk, c1)
+
+	rm1 := Decrypt(sk2, pk2, c2)
+
+	if strings.Compare(rm1.Num().String(), "100") != 0 {
+		t.Errorf("the generated token or key update mechanism changed the original message")
 	}
 }
