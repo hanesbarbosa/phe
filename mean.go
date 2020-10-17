77798873c6ddca658d9153fdb95b7d42f1562075
Hanes Oliveira <hanes.barbosa@gmail.com>
2020-10-16 22:12:43 -0600

    Creating a mean function that receives parameters as strings.

--- mean.go
+++ mean.go
@@ -16,3 +16,18 @@ func Mean(pk *PublicKey, ms []*Multivector) *Multivector {
 
 	return ScalarDivision(pk, tm, d)
 }
+
+// MeanFromString parses strings and invokes Mean.
+func MeanFromString(m string, ms []string) string {
+	q := big.NewInt(0)
+	q.SetString(m, 10)
+	pk := &PublicKey{Q: q}
+	var mv []*Multivector
+	n := len(ms)
+
+	for i := 0; i < n; i++ {
+		mv = append(mv, StringToMultivector(ms[i]))
+	}
+
+	return Mean(pk, mv).ToString()
+}
--- mean_test.go
+++ mean_test.go
@@ -28,3 +28,27 @@ func TestMean(t *testing.T) {
 		t.Errorf("The average is not correct!")
 	}
 }
+
+func TestMeanFromString(t *testing.T) {
+	sk, pk := GenerateKeys(256)
+
+	m1 := big.NewInt(10)
+	m2 := big.NewInt(20)
+	m3 := big.NewInt(30)
+	m4 := big.NewInt(40)
+
+	c1 := Encrypt(sk, pk, m1)
+	c2 := Encrypt(sk, pk, m2)
+	c3 := Encrypt(sk, pk, m3)
+	c4 := Encrypt(sk, pk, m4)
+
+	ms := []string{c1.ToString(), c2.ToString(), c3.ToString(), c4.ToString()}
+	m := MeanFromString(pk.Q.String(), ms)
+
+	mm := StringToMultivector(m)
+	rm := Decrypt(sk, pk, mm)
+
+	if strings.Compare(rm.String(), "25/1") != 0 {
+		t.Errorf("The average is not correct!")
+	}
+}
